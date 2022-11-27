ff7015de3a13effe87fe85559ec409a6e04dc525
Zubad <zubad.ali95@gmail.com>
2022-11-27 18:18:27 +0500

    javascript for calculator completed.

--- script.js
+++ script.js
@@ -1,34 +1,65 @@
-const form = document.getElementById('calc_form');
-const output = document.getElementById('output');
-const operand_btn = document.querySelectorAll('button[data-type=operand]');
-const operator_btn = document.querySelectorAll('button[data-type=operator]')
+const output = document.getElementById("output");
+const form = document.getElementById("calc_form");
+const operand_btns = document.querySelectorAll("button[data-type=operand]");
+const operator_btns = document.querySelectorAll("button[data-type=operator]");
 
 form.addEventListener("submit", (e) => {
- e.preventDefault();
+  e.preventDefault();
 });
 
-let isOperator = false;
+let is_operator = false;
+let equation = [];
 
-operand_btn.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        if(output.value == '0'){
-            output.value = e.target.value;
-        } else if (output.value.includes(".")){
-            output.value = output.value + '' + e.target.value.replace('.', '');
-        } else if(isOperator){
-            isOperator = true;
-            output.value = e.target.value;
+const remove_active = () => {
+  operator_btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+};
+
+operand_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    remove_active();
+    if (output.value == "0") {
+      output.value = e.target.value;
+    } else if (output.value.includes(".")) {
+      output.value = output.value + "" + e.target.value.replace(".", "");
+    } else if (is_operator) {
+      is_operator = false;
+      output.value = e.target.value;
+    } else {
+      output.value = output.value + "" + e.target.value;
+    }
+  });
+});
+
+operator_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    remove_active();
+    e.currentTarget.classList.add("active");
+
+    switch (e.target.value) {
+      case "%":
+        output.value = parseFloat(output.value) / 100;
+        break;
+      case "invert":
+        output.value = parseFloat(output.value) * -1;
+        break;
+      case "=":
+        equation.push(output.value);
+        output.value = eval(equation.join(""));
+        equation = [];
+        break;
+      default:
+        let last_item = equation[equation.length - 1];
+        if (["/", "*", "+", "-"].includes(last_item) && is_operator) {
+          equation.pop();
+          equation.push(e.target.value);
         } else {
-            output.value = output.value + "" + e.target.value;
+          equation.push(output.value);
+          equation.push(e.target.value);
         }
-    })
-})
-
-operator_btn.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        
-    })
-})
-
-
-
+        is_operator = true;
+        break;
+    }
+  });
+});
